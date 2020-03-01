210729aee923a130f11e2c2f40f605d6267b166d
JahongirD <jahon15206@gmail.com>
2020-03-01 13:55:19 -0500

    create basic navigation and tested browser

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -2,11 +2,12 @@ package com.automation.tests.day2;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //to start selenium script we need:
         //setup webdriver and vreate webdriver object
@@ -15,5 +16,54 @@ public class BasicNavigation {
         WebDriver driver = new ChromeDriver();
         // In selenium, everything starts from WebDriver interface
         driver.get("http://google.com"); // to open a website
+        Thread.sleep(3000);
+        
+        
+        String title = driver.getTitle(); // returns <title> some title </title>
+        System.out.println("title = " + title);
+        String expectedTitle = "Google";
+        if(expectedTitle.equals(title)){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        driver.close();
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
